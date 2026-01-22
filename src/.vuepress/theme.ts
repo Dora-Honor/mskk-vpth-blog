4bfeca6cb6405416525d27108c50266322ccc592
Dora-Honor <dorahonor@gmail.com>
2026-01-23 03:58:05 +0900

    Upgrade packages & change Waline URL

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -163,7 +163,10 @@ export default hopeTheme({
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
     comment: {
       provider: "Waline",
-      serverURL: "https://waline.dorahonor.me/.netlify/functions/comment",
+      serverURL: "https://comment.dorahonor.me/",
+      // serverURL1: "https://comment.dorahonor.me/",
+      // serverURL2: "https://vercel-waline.dorahonor.me/",
+      // serverURL3: "https://waline.dorahonor.me/.netlify/functions/comment",
       dark: "auto",
       reaction: true,
       requiredMeta: ["nick", "mail"],
